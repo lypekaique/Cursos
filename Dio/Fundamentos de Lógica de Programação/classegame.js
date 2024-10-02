fa0d8a443da647d9a8098748e7dd6f1bc9de57a1
lypekaique <lypekaique@hotmail.com>
2024-10-02 11:05:53 -0300

    new file:   "Dio/Fundamentos de L\303\263gica de Programa\303\247\303\243o/classegame.js"

---
+++ Dio/Fundamentos de Lógica de Programação/classegame.js
@@ -0,0 +1,31 @@
+class Hero {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+
+    switch (this.tipo.toLowerCase()) {
+      case "mago":
+        ataque = "usou magia";
+        break;
+      case "guerreiro":
+        ataque = "usou espada";
+        break;
+      case "monge":
+        ataque = "usou artes marciais";
+        break;
+      case "ninja":
+        ataque = "usou shuriken";
+        break;
+      default:
+        ataque = "não tem ataque definido";
+        break;
+    }
+
+    console.log(`o ${this.tipo} atacou usando ${ataque}`);
+  }
+}
